6b2fd5b62de05b7b604fb2f22639b59aa9ae92d0
shriramrajat <rvshriram@dpcoepune.edu.in>
2026-01-06 01:56:40 +0530

    Add initial FastAPI backend and React frontend structure with product management features

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi import FastAPI
+from models import Products
+
+app=FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Hello World"}
+def greet():
+      return "Hello World" 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+products=[
+     Products(id=1,name="Phone",description="Budget Phone",price=999,quantity=50),
+     Products(id=2,name="Laptop",description="Gaming Laptop",price=4999,quantity=30),
+     Products(id=3,name="Tablet",description="Smart Tablet",price=799,quantity=60),
+     Products(id=4,name="Headphones",description="Wireless Headphones",price=199,quantity=80)
+]
+
+@app.get("/products")
+def get_all_products():
+    return products
+
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id: int):
+      for product in products:
+           if product.id==product_id:
+                  return products[product_id-1]
+           
+      return {"message":"Product not found"}
+
+@app.post("/products")
+def add_new_product(new_product:Products):
+      products.append(new_product)
+      return new_product
+
+
